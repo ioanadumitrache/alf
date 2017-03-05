56d35d4a81d8c9e55b0b244ffe97736f173295b6
Alexandru Radovici <msg4alex@gmail.com>
2017-03-05 15:38:08 +0000

    TP1

---
+++ TP1/exercise1.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Exercise 1
+var nr = 10; // number variable
+var str = "a string"; // string variable
+var fnr = 10.5; // number variable
+var tf = false; // boolean variable
+
---
+++ TP1/exercise2.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Exercise 2
+var nr = 10; // number variable
+var str = "a string"; // string variable
+var fnr = 10.5; // number variable
+var tf = false; // boolean variable
+
+console.log (typeof (nr));
+console.log (typeof (str));
+console.log (typeof (fnr));
+console.log (typeof (tf));
